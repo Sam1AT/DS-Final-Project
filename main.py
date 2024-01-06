cb26bf60c7d151b54ea0dd955e4a3b5e268e660e
Sam1AT <alileon32@gmail.com>
2024-01-06 17:46:01 +0330

    add : main app to project

---
+++ main.py
@@ -0,0 +1,6 @@
+from utils import *
+
+opt = 1
+while opt:
+    print('Hi , welcome to shajare name app! This app is designed by Sam and Mehrana \n Choose an option:')
+    print('    1) Create a new tree')
